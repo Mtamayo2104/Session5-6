c1033ed2ff02b3ebf15d0eea8879997aeca7b2fa
Mtamayo2104 <miguel.tamayo2104@gmail.com>
2025-01-22 16:55:04 +0100

    iput examples

---
+++ Session5&6.py
@@ -0,0 +1,4 @@
+name = input("what is your name? ")
+print("Hello", name)
+age = input("How old are you? ")
+print("you were probably born in ", 2024-int(age)) # u need to specify the value as integer to avoid errors
